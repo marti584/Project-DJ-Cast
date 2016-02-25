0eac6271363541609b5ff0954f93524605dd72f3
lheilman <lheilman@purdue.edu>
2016-02-25 17:18:41 -0500

    [Feat]Working play/pause and skip buttons for YouTube videos

--- client/templates/public/Admin.js
+++ client/templates/public/Admin.js
@@ -1,23 +1,43 @@
-var tag = document.createElement('script');
-
-tag.src= "http://www.youtube.com/iframe_api";
-var firstScriptTag = document.getElementsByTagName('script')[0];
-firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-
-var player;	
-
-
-
-
-var queue = [];
-Template.Admin.onCreated(function(){
+/*Template.Admin.onCreated(function(){
+	var queue = [];
 	console.log("Created");
 	queue.push('KS7UW_-toBk');
 	queue.push('fzllVlzzeuo');
 	queue.push('Jn2PNlhvy8E');
 	queue.push('W1PNvopXjbg');
 
+	var player;
+	onYouTubeIframeAPIReady = function () {
+	console.log("geting to here\n");
+		player = new YT.Player("player", {
+	    height: "600", 
+		  width: "400", 
+			videoId: "0CFuCYNx-1g", 
+      playerVars: {
+				'autoplay': 0,
+				'controls': 0,
+				'disablekb': 1,
+				'enablejsapi': 1,
+				'origin':"https://levistest.meteor.com/Admin",
+				'rel': 0
+			},
+			events: {
+	     	onReady: function (event){
+					//event.target.playVideo();
+					console.log("Firing!");
+				},
+				onStateChange: onPlayerStateChange
+      }
+    });
+	};
+
+	function onPlayerStateChange (event) {
+		if (event.data == YT.PlayerState.ENDED){
+			player.loadVideoById(queue.shift(), 0, "default");
+			//Video Ended
+
+		}
+	}
 	YT.load();
 
 
@@ -43,35 +63,5 @@ Template.Admin.onCreated(function(){
 });
 
 
-onYouTubeIframeAPIReady = function () {
-	console.log("geting to here\n");
-		player = new YT.Player("player", {
-	    height: "600", 
-		  width: "400", 
-			videoId: "0CFuCYNx-1g", 
-      playerVars: {
-				'autoplay': 0,
-				'controls': 0,
-				'disablekb': 1,
-				'enablejsapi': 1,
-				'origin':"https://levistest.meteor.com/Admin",
-				'rel': 0
-			},
-			events: {
-	     	onReady: function (event){
-					//event.target.playVideo();
-					console.log("Firing!");
-				},
-				onStateChange: onPlayerStateChange
-      }
-    });
-	};
 
-function onPlayerStateChange (event) {
-		if (event.data == YT.PlayerState.ENDED){
-			player.loadVideoById(queue.shift(), 0, "default");
-			//Video Ended
-
-		}
-	}
-
+*/
