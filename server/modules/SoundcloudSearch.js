f1b70bb82e348dcf6372af37d871b3d0ed7de9f0
Hunter Martin <sunnyboy451@gmail.com>
2016-04-25 16:44:01 -0400

    Bringing in those files

---
+++ server/modules/SoundcloudSearch.js
@@ -0,0 +1,6 @@
+Soundcloud.setConfig({
+    client_id : "5dcb740fce5f81cd280292ecf8a1900c",
+    client_secret : "97e29ab8d2d4c05a117082ca1baebbcd",
+    username : 'phand@purdue.edu',
+    password: 'donphan123'
+  });
