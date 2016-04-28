71a55cad29c8c68087d857e53864e168df9e8e12
Hunter Martin <sunnyboy451@gmail.com>
2016-04-28 17:47:36 -0400

    Browserpolicy

--- packages/fixtures/browser_policy.js
+++ packages/fixtures/browser_policy.js
@@ -1,4 +1,5 @@
 BrowserPolicy.framing.allowAll();
 BrowserPolicy.content.allowOriginForAll("http://localhost:*");
+BrowserPolicy.content.allowOriginForAll("https://dj-cast.herokuapp.com");
 BrowserPolicy.content.allowOriginForAll("*");
 BrowserPolicy.content.allowImageOrigin("*");
