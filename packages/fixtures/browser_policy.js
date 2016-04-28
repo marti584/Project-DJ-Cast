a90bbf85d2dedda2e9a553781558b133a08367a1
Hunter Martin <sunnyboy451@gmail.com>
2016-04-28 18:04:41 -0400

    More browser stuff

--- packages/fixtures/browser_policy.js
+++ packages/fixtures/browser_policy.js
@@ -1,5 +1,6 @@
 BrowserPolicy.framing.allowAll();
 BrowserPolicy.content.allowOriginForAll("http://localhost:*");
-BrowserPolicy.content.allowOriginForAll("https://dj-cast.herokuapp.com");
+BrowserPolicy.content.allowOriginForAll("https://dj-cast.herokuapp.com/*");
+BrowserPolicy.content.allowDataUrlForAll("*");
 BrowserPolicy.content.allowOriginForAll("*");
 BrowserPolicy.content.allowImageOrigin("*");
